83f9224705c8f3799be39263c315b0ca32e025db
podenwald <odenwald.patrick@gmail.com>
2026-04-21 15:00:45 +0200

    App Vorbereitungen

---
+++ src/utils/version_MBP-von-test.fritz.box_Apr-20-113945-2026_Conflict.ts
@@ -0,0 +1,30 @@
+// Version information that gets injected at build time
+export interface VersionInfo {
+  version: string
+  gitTag: string | null
+  gitCommit: string
+  buildDate: string
+  isProduction: boolean
+}
+
+// This will be replaced by Vite during build
+export const VERSION_INFO: VersionInfo = {
+  "version": "1.0.7",
+  "gitTag": "v1.0.7",
+  "gitCommit": "09843ddf437bc7df13208bc887c71f71d9d4fd56",
+  "buildDate": "2026-04-19T17:26:46.997Z",
+  "isProduction": false
+}
+
+export function getDisplayVersion(): string {
+  if (VERSION_INFO.gitTag) {
+    return VERSION_INFO.gitTag
+  }
+  return 'Beta'
+}
+
+export function getFullVersionInfo(): string {
+  const displayVersion = getDisplayVersion()
+  const commit = VERSION_INFO.gitCommit.substring(0, 7)
+  return `${displayVersion} (${commit})`
+}

499faf5b73dff71c6c5b0217eac63c9b5d92fd9f
podenwald <odenwald.patrick@gmail.com>
2026-09-07 16:08:22 +0200

    Versions-Metadaten für v1.9.3 korrigieren

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -9,10 +9,10 @@ export interface VersionInfo {
 
 // This will be replaced by Vite during build
 export const VERSION_INFO: VersionInfo = {
-  "version": "1.9.2",
-  "gitTag": "v1.9.1",
-  "gitCommit": "c551ceca33abb4fcfae57e060230a543b28ca44d",
-  "buildDate": "2026-09-06T15:30:07.145Z",
+  "version": "1.9.3",
+  "gitTag": "v1.9.2",
+  "gitCommit": "a899dcb8a80c5ec78b1a151a239abab3cdcb11a2",
+  "buildDate": "2026-09-07T13:08:17.416Z",
   "isProduction": false
 }
 
